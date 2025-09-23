7d255c7a8652a66e10485b1dca93eba839028ccb
fberrizbeitia <fberrizbeitia@gmail.com>
2025-09-23 12:02:09 -0400

    update to  /etl/fetch.sh

--- etl/fetch.sh
+++ etl/fetch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Retrieving all partner institutions..."
+python3 ./fetch/get_partner_institutions.py
+echo "Done retrieving data."
+
 echo "Retrieving latest dataset..."
 python3 ./fetch/retriever.py
 echo "Done retrieving data."
